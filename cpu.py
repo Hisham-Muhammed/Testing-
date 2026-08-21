d6a477887182f8c8dfe15d8c4b2275c30694b094
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:31:19 +0530

    Introduce halted state in CPU class
    Added halted state to CPU class and updated methods.

--- cpu.py
+++ cpu.py
@@ -1,33 +1,77 @@
+from instructions import InstructionSet
+
+
 class CPU:
+
     def __init__(self):
-        # 8-bit registers R0 to R7
+
         self.registers = [0] * 8
 
-        # Accumulator
         self.A = 0
 
-        # Program Counter
         self.PC = 0
 
-        # Stack Pointer
         self.SP = 7
 
-        # Flags
         self.zero_flag = False
         self.carry_flag = False
+
+        self.halted = False
 
     def reset(self):
+
         self.registers = [0] * 8
+
         self.A = 0
         self.PC = 0
         self.SP = 7
+
         self.zero_flag = False
         self.carry_flag = False
 
+        self.halted = False
+
+    def execute(self, instruction):
+
+        operation = instruction[0]
+
+        if operation == "MOV":
+
+            register = instruction[1]
+            value = instruction[2]
+
+            InstructionSet.MOV(
+                self,
+                register,
+                value
+            )
+
+        elif operation == "ADD":
+
+            register = instruction[1]
+
+            InstructionSet.ADD(
+                self,
+                register
+            )
+
+        elif operation == "HALT":
+
+            InstructionSet.HALT(self)
+
+        else:
+
+            print("Unknown instruction:", operation)
+            return
+
+        self.PC += 1
+
     def show_state(self):
+
         print("----- CPU STATE -----")
 
         for i in range(8):
+
             print(f"R{i} =", self.registers[i])
 
         print("A  =", self.A)
@@ -36,5 +80,7 @@ class CPU:
 
         print("Zero Flag  =", self.zero_flag)
         print("Carry Flag =", self.carry_flag)
+
+        print("Halted     =", self.halted)
 
         print("---------------------")
