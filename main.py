ddd42e0107a11280242b7f8d277aaf46c18a2c1b
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:33:04 +0530

    Set up CPU and memory for MicroOS-Sim
    Initialized CPU with memory and added program execution loop.

--- main.py
+++ main.py
@@ -2,13 +2,48 @@ from cpu import CPU
 from memory import Memory
 
 
+# Create memory
 memory = Memory()
-cpu = CPU()
 
-print("MicroOS-Sim")
+
+# Create CPU
+cpu = CPU(memory)
+
+
+# Program
+program = [
+
+    ("MOV", 0, 10),
+
+    ("MOV", 1, 20),
+
+    ("ADD", 0),
+
+    ("HALT",)
+
+]
+
+
+# Load program into program memory
+
+for address, instruction in enumerate(program):
+
+    memory.write_program(
+        address,
+        instruction
+    )
+
+
+print("===== MicroOS-Sim =====")
 print()
 
-cpu.show_state()
 
-print()
-print("Memory initialized successfully.")
+# CPU execution loop
+
+while not cpu.halted:
+
+    cpu.step()
+
+    cpu.show_state()
+
+    print()
