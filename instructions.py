8f0503de0fedd555bb02b43283acabbc46df01bc
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:33:58 +0530

    Refactor zero_flag assignments for clarity

--- instructions.py
+++ instructions.py
@@ -2,15 +2,17 @@ class InstructionSet:
 
     @staticmethod
     def MOV(cpu, register, value):
+
+        value = value % 256
+
         cpu.registers[register] = value
 
-        if value == 0:
-            cpu.zero_flag = True
-        else:
-            cpu.zero_flag = False
+        cpu.zero_flag = (value == 0)
+
 
     @staticmethod
     def ADD(cpu, register):
+
         result = cpu.A + cpu.registers[register]
 
         if result > 255:
@@ -21,11 +23,52 @@ class InstructionSet:
 
         cpu.A = result
 
-        if cpu.A == 0:
-            cpu.zero_flag = True
+        cpu.zero_flag = (cpu.A == 0)
+
+
+    @staticmethod
+    def SUB(cpu, register):
+
+        result = cpu.A - cpu.registers[register]
+
+        if result < 0:
+            cpu.carry_flag = True
+            result = result % 256
         else:
-            cpu.zero_flag = False
+            cpu.carry_flag = False
+
+        cpu.A = result
+
+        cpu.zero_flag = (cpu.A == 0)
+
+
+    @staticmethod
+    def INC(cpu, register):
+
+        cpu.registers[register] += 1
+
+        if cpu.registers[register] > 255:
+            cpu.registers[register] = 0
+
+        cpu.zero_flag = (
+            cpu.registers[register] == 0
+        )
+
+
+    @staticmethod
+    def DEC(cpu, register):
+
+        cpu.registers[register] -= 1
+
+        if cpu.registers[register] < 0:
+            cpu.registers[register] = 255
+
+        cpu.zero_flag = (
+            cpu.registers[register] == 0
+        )
+
 
     @staticmethod
     def HALT(cpu):
+
         cpu.halted = True
