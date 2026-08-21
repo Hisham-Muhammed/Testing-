fe2cc7a00115f7a758d93b087df89d05c5c5d93d
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:50:54 +0530

    Refactor main.py to use CPU and Memory classes
    Refactor main.py to include CPU and Memory classes, and load a program into memory for execution.

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+from cpu import CPU
+from memory import Memory
+
+
+# Create memory
+memory = Memory()
+
+# Create CPU
+cpu = CPU(memory)
+
+
+# Program to execute
 program = [
-
     ("MOV", 0, 10),
-
     ("MOV", 1, 5),
-
     ("INC", 0),
-
     ("DEC", 1),
-
     ("ADD", 0),
-
     ("SUB", 1),
-
     ("HALT",)
-
 ]
+
+
+# Load program into program memory
+for address, instruction in enumerate(program):
+    memory.write_program(address, instruction)
+
+
+print("===== MicroOS-Sim =====")
+print()
+
+
+# Fetch-Decode-Execute cycle
+while not cpu.halted:
+
+    cpu.step()
+
+    cpu.show_state()
+
+    print()
