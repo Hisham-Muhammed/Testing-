1d5b060c06538ae1470ae120991fdf29e25d9fd6
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from cpu import CPU
+from memory import Memory
 
+
+memory = Memory()
 cpu = CPU()
 
+print("MicroOS-Sim")
+print()
+
 cpu.show_state()
+
+print()
+print("Memory initialized successfully.")
