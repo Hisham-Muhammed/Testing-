d83ba07d5f7f4943535d964a07ff7d782295ad71
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 22:02:00 +0530

    Refactor CPU program instructions and flow

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ from cpu import CPU
 from memory import Memory
 
 
-# Create memory
 memory = Memory()
 
-# Create CPU
 cpu = CPU(memory)
 
 
-# Program to test PUSH and POP
 program = [
 
     ("MOV", 0, 10),
 
-    ("PUSH", 0),
+    ("CALL", 4),
 
-    ("MOV", 1, 20),
+    ("HALT",),
 
-    ("POP", 1),
+    ("NOP",),
 
-    ("HALT",)
+    ("INC", 0),
+
+    ("RET",)
 
 ]
 
 
-# Load program into program memory
 for address, instruction in enumerate(program):
 
     memory.write_program(
@@ -38,7 +36,6 @@ print("===== MicroOS-Sim =====")
 print()
 
 
-# Fetch-Decode-Execute cycle
 while not cpu.halted:
 
     cpu.step()
