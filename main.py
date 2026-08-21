3ad96697d7c2f4b6423ea7a6c3bf5edfca794d41
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-08-21 21:35:06 +0530

    Refactor program instructions in main.py
    Refactor program instructions and remove CPU and memory initialization.

--- main.py
+++ main.py
@@ -1,49 +1,17 @@
-from cpu import CPU
-from memory import Memory
-
-
-# Create memory
-memory = Memory()
-
-
-# Create CPU
-cpu = CPU(memory)
-
-
-# Program
 program = [
 
     ("MOV", 0, 10),
 
-    ("MOV", 1, 20),
+    ("MOV", 1, 5),
+
+    ("INC", 0),
+
+    ("DEC", 1),
 
     ("ADD", 0),
+
+    ("SUB", 1),
 
     ("HALT",)
 
 ]
-
-
-# Load program into program memory
-
-for address, instruction in enumerate(program):
-
-    memory.write_program(
-        address,
-        instruction
-    )
-
-
-print("===== MicroOS-Sim =====")
-print()
-
-
-# CPU execution loop
-
-while not cpu.halted:
-
-    cpu.step()
-
-    cpu.show_state()
-
-    print()
